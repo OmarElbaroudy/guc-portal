a22b1c72ebcd4cc7bde2b5abb3c5bb018fbcda5c
Omar Elbaroudy <elbaroudyomar@gmail.com>
2020-12-13 21:36:41 +0200

    added login

--- index.js
+++ index.js
@@ -18,47 +18,40 @@ const cluster = 'mongodb+srv://admin:admin@cluster0.ryozj.mongodb.net/Proj?retry
 mongoose.connect(cluster, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
     .then(() => {
         app.use(express.json())
-        app.post('/login',async (req,res)=>{
-            const email=req.body.email;
-            const h=await HR.findOne({email:email})
-            if(!h){
-                const a=await Academic.findOne({email:email})
-                if(!a){
-                   return res.status(403).send("This email doesn't exist")
-                }
-                else{
-                    const verified=await bcrypt.compare(req.body.password,h.password)
-                    if(!verified){
-                        return res.status(403).send("wrong password")
-                    }
-                    const payload={email:a.email,type:"academic"}
-                    const token=jwt.sign(payload,key)
-                    res.header('auth-token',token)
-                    res.send("login successfull")
+        app.post('/login', async (req, res) => {
+            const email = req.body.email;
+            const h = await HR.findOne({email: email})
+            const a = await Academic.findOne({email: email})
 
-                }
+            if (!h && !a) {
+                return res.status(403).send("This email doesn't exist");
             }
-            else{
-                const verified=await bcrypt.compare(req.body.password,h.password)
-                if(!verified){
-                    return res.status(403).send("wrong password")
-                }
-                const payload={email:h.email,type:"hr"}
-                const token=jwt.sign(payload,key)
-                res.header('auth-token',token)
-                res.send("login successfull")
+
+            const user = h ? h : a;
+            const verified = await bcrypt.compare(req.body.password, user.password)
+
+            if (!verified) {
+                return res.status(403).send("wrong password")
             }
+
+            const payload = {id: user.id, type: h ? "hr" : "academic"}
+            const token = jwt.sign(payload, key)
+
+            res.header('auth-token', token)
+            res.send("login successfull")
+
         })
 
         function authenticate(req, res, next) {
             if (!req.header('auth-token')) {
-                return res.status(403).send("el token ya 7aramy")
+                return res.status(403).send("unauthorized access")
             }
+
             try {
                 jwt.verify(req.header('auth-token'), key)
                 next()
             } catch (err) {
-                res.status(403).send("el token msh beta3ak")
+                res.status(403).send("invalid token")
             }
         }
 
