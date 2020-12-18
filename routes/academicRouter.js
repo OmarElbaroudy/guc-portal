a4d71fb91228aeb1dd74440c3f91f22e08aa14f3
Omar Elbaroudy <elbaroudyomar@gmail.com>
2020-12-19 01:27:47 +0200

    academicRouter finalized

--- routes/academicRouter.js
+++ routes/academicRouter.js
@@ -17,7 +17,8 @@ const dateDiff = (slotDate, curDate) => {
 };
 
 const getCourseNameById = async (id) => {
-	return await courses.findById(id).name;
+	const ret = await courses.findById(id).select("name -_id");
+	return ret ? ret.name : undefined;
 };
 
 const getCourseIdByName = async (name) => {
@@ -26,12 +27,17 @@ const getCourseIdByName = async (name) => {
 };
 
 const getLocationNameById = async (id) => {
-	return await locations.findById(id).name;
+	const ret = await locations.findById(id).select("name -_id");
+	return ret ? ret.name : undefined;
 };
 
 const getLocationIdByName = async (name) => {
 	const ret = await locations.findOne({ name: name });
 	return ret ? ret._id : undefined;
+};
+
+const getId = async (_id) => {
+	return (await academics.findById(_id).select("id")).id;
 };
 
 const getCurDate = () => {
@@ -62,7 +68,7 @@ router.get("/ac/viewSchedule", auth, async (req, res) => {
 		let sessions = [];
 		let curDate = getCurDate();
 
-		for (const reqID of cur.receivedRequests) {
+		for (const reqID of cur.receivedRequestsId) {
 			const request = await requests.findOne({
 				_id: reqID,
 			});
@@ -78,8 +84,8 @@ router.get("/ac/viewSchedule", auth, async (req, res) => {
 				let session = {
 					slot: request.replacement.slot,
 					weekDay: request.replacement.slotDate.getDay(),
-					location: await getLocationNameById(request.replacement.location),
-					course: await getCourseNameById(request.replacement.course),
+					location: await getLocationNameById(request.replacement.locationId),
+					course: await getCourseNameById(request.replacement.courseId),
 				};
 
 				sessions.push(session);
@@ -90,7 +96,7 @@ router.get("/ac/viewSchedule", auth, async (req, res) => {
 			sessions.push({
 				slot: session.slot,
 				weekDay: session.weekDay,
-				location: await getLocationNameById(session.location),
+				location: await getLocationNameById(session.locationId),
 				course: await getCourseNameById(session.courseId),
 			});
 		}
@@ -101,6 +107,14 @@ router.get("/ac/viewSchedule", auth, async (req, res) => {
 	}
 });
 
+// {
+// 	"id" : "ac-10",
+// 	"slot": "2",
+// 	"weekDay" : "1",
+// 	"slotDate" : { "year" : "2020" ,  "month" : "12", "day" : "21"},
+// 	"course" : "CSEN101"
+// }
+
 router
 	.route("/ac/replacement")
 	.post(auth, async (req, res) => {
@@ -110,10 +124,11 @@ router
 			const input = req.body; //id receiver, slot, location, {year, month, day}, course
 
 			const sender = await academics.findById(decoded.id);
-			const receiver = await academics.findById(input.id);
+			const receiver = await academics.findOne({ id: input.id });
 			const courseId = await getCourseIdByName(input.course);
 
 			if (!receiver) return res.status(405).send("invalid receiver id");
+			if (!courseId) return res.status(416).send("no such course");
 
 			let fst = false;
 			sender.courses.forEach((item) => {
@@ -126,9 +141,8 @@ router
 			});
 
 			if (!fst || !snd) return res.status(406).send("invalid course");
-
 			let slotDate = new Date(
-				Date.UTC(input.day.year, input.day.month - 1, input.day.day)
+				Date.UTC(input.slotDate.year, input.slotDate.month - 1, input.slotDate.day)
 			);
 
 			let weekDay = slotDate.getDay();
@@ -145,13 +159,13 @@ router
 
 			const Request = {
 				status: "pending",
-				type: "Replacement",
-				department: sender.department,
+				type: "replacement",
+				departmentId: sender.departmentId,
 				senderId: sender._id,
 				receiverId: receiver._id,
 				issueDate: getCurDate(),
 				replacement: {
-					course: courseId,
+					courseId: courseId,
 					slot: input.slot,
 					locationId: await getLocationIdByName(input.location),
 					slotDate: slotDate,
@@ -162,8 +176,8 @@ router
 			const arr = await requests.insertMany(Request);
 			const reqID = arr[0]._id;
 
-			sender.sentRequests.push(reqID);
-			receiver.receivedRequests.push(reqID);
+			sender.sentRequestsId.push(reqID);
+			receiver.receivedRequestsId.push(reqID);
 
 			await sender.save();
 			await receiver.save();
@@ -184,7 +198,7 @@ router
 
 			if (!sender) return res.status(408).send("no such academic member");
 
-			for (const reqID of sender.sentRequests) {
+			for (const reqID of sender.sentRequestsId) {
 				const request = await requests.findOne({
 					_id: reqID,
 					type: "replacement",
@@ -193,38 +207,39 @@ router
 				if (!request) res.status(409).send("no requests");
 
 				let replacementReq = {
-					course: await getCourseNameById(request.replacement.course),
+					course: await getCourseNameById(request.replacement.courseId),
 					slotDate: request.replacement.slotDate,
 					slot: request.replacement.slot,
-					location: await getLocationNameById(request.replacement.location),
-					status: request.replacement.status,
+					location: await getLocationNameById(request.replacement.locationId),
+					status: request.replacement.academicResponse,
 					hodStatus: request.status,
-					receiver: request.receiver,
+					receiver: await getId(request.receiverId),
 					issueDate: request.issueDate,
 				};
 
 				replacements.push(replacementReq);
 			}
 
-			for (const reqID of sender.receivedRequests) {
+			for (const reqID of sender.receivedRequestsId) {
 				const request = await requests.findOne({
 					_id: reqID,
-					type: "Replacement",
+					type: "replacement",
 				});
 
 				let replacementReq = {
-					course: await getCourseNameById(request.replacement.course),
+					course: await getCourseNameById(request.replacement.courseId),
 					slotDate: request.replacement.slotDate,
 					slot: request.replacement.slot,
-					location: await getLocationNameById(request.replacement.location),
-					status: request.replacement.status,
+					location: await getLocationNameById(request.replacement.locationId),
+					status: request.replacement.academicResponse,
 					hodStatus: request.status,
-					sender: request.sender,
+					sender: await getId(request.senderId),
 					issueDate: request.issueDate,
 				};
 
 				replacements.push(replacementReq);
 			}
+
 			res.send(replacements);
 		} catch (err) {
 			console.log(err);
@@ -234,11 +249,11 @@ router
 //TODO:
 //check for valid inputs
 
-router.post("/ac/slotRequest", auth, async (req, res) => {
+router.post("/ac/slotLinkingRequest", auth, async (req, res) => {
 	try {
 		const token = req.header("auth-token");
 		const decoded = jwt_decode(token);
-		const input = req.body; //slot, weekday and course in req.body
+		const input = req.body; //slot, weekday and courseName in req.body
 
 		const sender = await academics.findById(decoded.id);
 		if (!sender) res.status(410).send("invalid id");
@@ -246,7 +261,7 @@ router.post("/ac/slotRequest", auth, async (req, res) => {
 		let flag = false;
 		for (const course of sender.courses) {
 			const courseName = await getCourseNameById(course.courseId);
-			if (courseName === input.name && course.position !== "coordinator")
+			if (courseName === input.courseName && course.position !== "coordinator")
 				flag = true;
 		}
 
@@ -256,20 +271,30 @@ router.post("/ac/slotRequest", auth, async (req, res) => {
 			name: input.courseName,
 		});
 
+		if (!course) res.status(411).send("invalid course");
+
+		flag = false;
+		for (const session of course.schedule) {
+			flag |=
+				!session.instructorId &&
+				session.weekDay === input.weekDay &&
+				session.slot === input.slot;
+		}
+
+		if (!flag) res.status(417).send("slot is not available for linkage");
+
 		const coordinatorId = course.coordinatorId;
 		const coordinator = await academics.findById(coordinatorId);
 
-		if (!course) res.status(411).send("invalid course");
-
-		let curDate = getCurDate();
 		const request = {
 			status: "pending",
 			type: "slotLinking",
-			sender: sender._id,
-			receiver: coordinatorId,
-			issueDate: curDate,
+			senderId: sender._id,
+			receiverId: coordinatorId,
+			issueDate: getCurDate(),
+			departmentId: sender.departmentId,
 			slotLinking: {
-				course: course._id,
+				courseId: course._id,
 				slot: input.slot,
 				weekDay: input.weekDay,
 			},
@@ -278,8 +303,8 @@ router.post("/ac/slotRequest", auth, async (req, res) => {
 		const arr = await requests.insertMany(request);
 		const reqID = arr[0]._id;
 
-		sender.sentRequests.push(reqID);
-		coordinator.receivedRequests.push(reqID);
+		sender.sentRequestsId.push(reqID);
+		coordinator.receivedRequestsId.push(reqID);
 
 		await sender.save();
 		await coordinator.save();
@@ -297,33 +322,36 @@ router.post("/ac/changeDayOff", auth, async (req, res) => {
 
 		const sender = await academics.findById(decoded.id);
 
-		const newDayOff = req.body.newDayOff;
+		const newDayOff = parseInt(req.body.newDayOff);
 		const comment = req.body.comment ? req.body.comment : "";
 
-		if (newDayOff < 0 || newDayOff > 6)
-			return res.status(412).send("invalid day, off day must be between 0 and 6");
+		if (newDayOff < 0 || newDayOff > 6 || newDayOff === 5)
+			return res
+				.status(412)
+				.send("invalid day, off day must be between 0 and 6 and not 5");
 
-		if (newDayOff === sender.day_off)
+		if (newDayOff === sender.dayOff)
 			return res.send("this is already your day off");
 
-		const hodId = await departments.findById(sender.department).hodId;
+		const hodId = (await departments.findById(sender.departmentId)).hodId;
 		const hod = await academics.findById(hodId);
 
 		const request = {
 			status: "pending",
 			type: "changeDayOff",
-			sender: sender._id,
-			receiver: hodId,
+			senderId: sender._id,
+			receiverId: hodId,
 			issueDate: getCurDate(),
 			newDayOff: newDayOff,
 			senderComment: comment,
+			departmentId: sender.departmentId,
 		};
 
 		const arr = await requests.insertMany(request);
 		const reqID = arr[0]._id;
 
-		sender.sentRequests.push(reqID);
-		hod.receivedRequests.push(reqID);
+		sender.sentRequestsId.push(reqID);
+		hod.receivedRequestsId.push(reqID);
 
 		await sender.save();
 		await hod.save();
@@ -336,121 +364,75 @@ router.post("/ac/changeDayOff", auth, async (req, res) => {
 router.post("/ac/leaveRequest", auth, async (req, res) => {
 	try {
 		//maternity, accidental, sick, compensation
-		//targetDay,
+		//date,
+		const input = req.body;
 		const token = req.header("auth-token");
 		const decoded = jwt_decode(token);
-		const input = req.body;
 		const comment = req.body.comment ? req.body.comment : "";
-		let request = [];
-		const sender = await academics.findOne({ id: decoded.id });
+
+		const sender = await academics.findById(decoded.id);
 		if (!sender) return res.status(413).send("invalid id");
 
 		const departmentId = sender.departmentId;
-		const hodId = await departments.findById(departmentId).hodId;
-		const hod = await academics.findOne({ id: hodId });
+		const hodId = (await departments.findById(departmentId)).hodId;
+		const hod = await academics.findById(hodId);
+
 		let curDate = getCurDate();
 		const type = input.type;
-		//maternity => check for female , proof documents
-		if (type === "maternity") {
-			if (sender.gender === "male") {
-				return res.status(413).send("males cannot submit maternity leaves");
-			} else {
-				request = {
-					Status: "pending",
-					type: "maternity",
-					senderComment: comment,
-					departmentId: departmentId,
-					senderId: sender.id,
-					receiverId: hod.id,
-					issue_date: curDate,
-					targetDate: input.targetDate,
-				};
-			}
+		const targetDate = new Date(
+			Date.UTC(input.date.year, input.date.month - 1, input.date.day)
+		);
+
+		if (type === "maternity" && sender.gender === "male") {
+			return res.status(413).send("males cannot submit maternity leaves");
 		}
-		//accidental => check accidental leave balance and annual leave balance
-		//deduct the balance in hod
-		if (type === "accidental") {
-			if (
-				sender.accidentalLeaveBalance.balance <= 0 ||
-				sender.annualLeaveBalance.balance <= 0
-			) {
-				return res.status(414).send("you consumed all your balance");
-			} else {
-				request = {
-					Status: "pending",
-					type: "accidental",
-					senderComment: comment,
-					departmentId: departmentId,
-					senderId: sender.id,
-					receiverId: hod.id,
-					issue_date: curDate,
-					targetDate: input.targetDate,
-				};
-			}
+
+		if (
+			type === "accidental" &&
+			(sender.accidentalLeaveBalance.balance <= 0 ||
+				sender.annualLeaveBalance.balance <= 0)
+		) {
+			return res.status(414).send("you consumed all your balance");
 		}
-		//sick => current date <= target day + 3,  proof documents
-		if (type === "sick") {
-			if (dateDiff(curDate, input.targetDate) > 3) {
-				return res
-					.status(415)
-					.send("you cannot submit a sick leave after more than 3 days");
-			}
-		} else {
-			request = {
-				Status: "pending",
-				type: "sick",
-				senderComment: comment,
-				departmentId: departmentId,
-				senderId: sender.id,
-				receiverId: hod.id,
-				issue_date: curDate,
-				targetDate: input.targetDate,
-			};
+
+		if (type === "sick" && dateDiff(curDate, input.targetDate) > 3) {
+			return res
+				.status(415)
+				.send("you cannot submit a sick leave after more than 3 days");
 		}
-		//compensation
-		if (type === "compensation") {
-			if (comment != "") {
-				request = {
-					Status: "pending",
-					type: "compensation",
-					senderComment: comment,
-					departmentId: departmentId,
-					senderId: sender.id,
-					receiverId: hod.id,
-					issue_date: curDate,
-					targetDate: input.targetDate,
-				};
-			} else {
-				return res.status(416).send("compensation leaves must have a reason");
-			}
+
+		if (type === "compensation" && !comment) {
+			return res.status(416).send("compensation leaves must have a reason");
 		}
-		
+
 		if (type === "annual") {
 			if (sender.annualLeaveBalance.balance <= 0) {
 				return res.status(416).send("you consumed all your annual leaves");
 			}
+
 			if (dateDiff(input.targetDate, curDate) <= 0) {
 				return res
 					.status(416)
 					.send("Annual leaves should be submitted before the targeted day");
 			}
-			request = {
-				Status: "pending",
-				type: "annual",
-				senderComment: comment,
-				departmentId: departmentId,
-				senderId: sender.id,
-				receiverId: hod.id,
-				issue_date: curDate,
-				targetDate: input.targetDate,
-			};
 		}
+
+		const request = {
+			status: "pending",
+			type: type,
+			senderComment: comment,
+			departmentId: departmentId,
+			senderId: sender._id,
+			receiverId: hod._id,
+			issueDate: curDate,
+			targetDate: targetDate,
+		};
 
 		const arr = await requests.insertMany(request);
 		const reqID = arr[0]._id;
 
-		sender.sentRequests.push(reqID);
-		hod.receivedRequests.push(reqID);
+		sender.sentRequestsId.push(reqID);
+		hod.receivedRequestsId.push(reqID);
 
 		await sender.save();
 		await hod.save();
@@ -471,7 +453,7 @@ router.post("/ac/viewSubmittedRequests", auth, async (req, res) => {
 
 		const sender = await academics.findById(decoded.id);
 		if (!sender) return res.status(413).send("invalid id");
-		const reqID = sender.sentRequests;
+		const reqID = sender.sentRequestsId;
 
 		if (status === "all") {
 			return academics.find({
@@ -496,14 +478,12 @@ router.post("/ac/cancelRequest", auth, async (req, res) => {
 		const sender = await academics.findById(decoded.id);
 		if (!sender) return res.status(413).send("invalid id");
 
-		const del = requests.find({
-			_id: req.body._id,
-		});
-
+		const del = requests.findById(req.body.reqId);
 		if (!del) return res.status(414).send("no such request");
+
 		const curDate = getCurDate();
 
-		if (!sender.sent_requests.includes(del._id))
+		if (!sender.sentRequestsId.includes(del._id))
 			return res.status(415).send("user not owner of request");
 
 		let flag = false;
@@ -518,38 +498,47 @@ router.post("/ac/cancelRequest", auth, async (req, res) => {
 		if (flag) {
 			const delID = del._id;
 			if (del.type === "replacement") {
-				const hodID = await departments.find({
-					name: del.department,
-				}).hod_ID;
+				const department = await departments.find({
+					_id: del.departmentId,
+				});
+
+				if (!department) {
+					return res.status(418).send("couldn't find department");
+				}
 
 				const hod = await academics.findOne({
-					id: hodID,
+					_id: department.hodId,
 				});
 
-				const idx = hod.received_requests.indexOf(delID);
-				if (idx > -1) {
-					hod.received_requests.splice(idx, 1);
-				}
+				if (hod) {
+					const idx = await hod.receivedRequestsId.indexOf(delID);
 
-				await hod.save();
+					if (idx > -1) {
+						await hod.receivedRequestsId.splice(idx, 1);
+					}
+
+					await hod.save();
+				}
 			}
 
-			if (del.receiver) {
+			if (del.receiverId) {
 				const receiver = await academics.findOne({
-					id: del.receiver,
+					id: del.receiverId,
 				});
 
-				const idx = receiver.received_requests.indexOf(delID);
+				if (receiver) {
+					const idx = await receiver.receivedRequestsId.indexOf(delID);
 
-				if (idx > -1) {
-					receiver.received_requests.splice.indexOf(idx, 1);
+					if (idx > -1) {
+						await receiver.receivedRequestsId.splice.indexOf(idx, 1);
+					}
+
+					await receiver.save();
 				}
-
-				await receiver.save();
 			}
 
-			const idx = sender.sent_requests.indexOf(delID);
-			sender.sent_requests.splice(idx, 1);
+			const idx = await sender.sentRequestsId.indexOf(delID);
+			await sender.sentRequestsId.splice(idx, 1);
 
 			await sender.save();
 			await requests.deleteOne({ _id: delID });
