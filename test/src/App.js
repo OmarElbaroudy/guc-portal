539880fed37a85e201c84d483e05e48b6df2c592
abouhegaziah <74408015+abouhegaziah@users.noreply.github.com>
2020-12-28 00:03:00 +0200

    login page

---
+++ test/src/App.js
@@ -0,0 +1,51 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Badge from 'react-bootstrap/Badge'
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import Form from 'react-bootstrap/Form'
+import Image from 'react-bootstrap/Image'
+
+
+function App() {
+
+  return (
+<body class="st">
+<div class="container">
+     <div class="row justify-content-center">
+     <img src="https://www.guc.edu.eg/img/guc_logo_og.png" class="img-circle"/>
+    </div>
+     <div class="row justify-content-center">
+      <div class="col-xl-6 col-lg-6 col-md-8 col-sm-12 col-md-xs-12">
+        <div class="form-container">
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                enter your @guc mail.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button class="btn btn-dark" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div> 
+  </div>
+</body>
+  
+
+  );
+}
+
+export default App;
