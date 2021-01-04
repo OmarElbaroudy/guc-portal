2576310da10f8b8821e2d68b17024aae0494783b
abouhegaziah <74408015+abouhegaziah@users.noreply.github.com>
2021-01-04 18:09:06 +0200

    baroudy

--- client/src/components/Cell.jsx
+++ client/src/components/Cell.jsx
@@ -4,38 +4,38 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import getterFetcher from "../API/getterFetcher";
 
 const Cell = (props) => {
-	const session = props.session;
-	const [courseName, setCourseName] = useState("");
-	const [locationName, setLocationName] = useState("");
-	const [type, setType] = useState("");
-	const [updated, setUpdated] = useState(false);
+  const session = props.session;
+  const [courseName, setCourseName] = useState("");
+  const [locationName, setLocationName] = useState("");
+  const [type, setType] = useState("");
+  const [updated, setUpdated] = useState(false);
 
-	if (session && session.courseId) {
-		const name = getterFetcher.getCourseNameById(session.courseId);
-		setCourseName(name);
-		setUpdated(true);
-	}
+  if (session && session.courseId) {
+    const name = getterFetcher.getCourseNameById(session.courseId);
+    setCourseName(name);
+    setUpdated(true);
+  }
 
-	if (session && session.locationId) {
-		const name = getterFetcher.getLocationNameById(session.locationId);
-		setLocationName(name);
-		setUpdated(true);
-	}
+  if (session && session.locationId) {
+    const name = getterFetcher.getLocationNameById(session.locationId);
+    setLocationName(name);
+    setUpdated(true);
+  }
 
-	if (session && session.type) {
-		setType(session.type);
-		setUpdated(true);
-	}
+  if (session && session.type) {
+    setType(session.type);
+    setUpdated(true);
+  }
 
-	return updated ? (
-		<Jumbotron fluid>
-			<td>
-				<h1>{courseName}</h1>
-				<h2>{locationName}</h2>
-				<h4>{type}</h4>
-			</td>
-		</Jumbotron>
-	) : null;
+  return updated ? (
+    <Jumbotron fluid>
+      <td>
+        <h1>{courseName}</h1>
+        <h2>{locationName}</h2>
+        <h4>{type}</h4>
+      </td>
+    </Jumbotron>
+  ) : null;
 };
 
 export default Cell;
