e9eb635e7d8e1ff3b640e1d4077fbd96c4c7a669
Omar Elbaroudy <elbaroudyomar@gmail.com>
2021-01-15 01:06:31 +0200

    api heroku app

--- client/src/API/loginFetcher.js
+++ client/src/API/loginFetcher.js
@@ -1,38 +1,38 @@
 const port = process.env.PORT || 8000;
 export class loginFetcher {
-  static async login(email, password) {
-    try {
-      const params = { email: email, password: password };
-      const res = await fetch("http://localhost:"+ port +"/login", {
-        method: "POST",
-        body: JSON.stringify(params),
-        headers: {
-          Authorization: "",
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      });
+	static async login(email, password) {
+		try {
+			const params = { email: email, password: password };
+			const res = await fetch("https://gucportal.herokuapp.com", {
+				method: "POST",
+				body: JSON.stringify(params),
+				headers: {
+					Authorization: "",
+					"Content-Type": "application/json",
+					Accept: "application/json",
+				},
+			});
 
-      const data = await res.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  static async AddFirst() {
-    try {
-      const res = await fetch("http://localhost:"+ port +"/createHr", {
-        method: "get",
-        headers: {
-          Authorization: "",
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      });
-      const data = await res.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  }
+			const data = await res.json();
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	static async AddFirst() {
+		try {
+			const res = await fetch("https://gucportal.herokuapp.com/createHr", {
+				method: "get",
+				headers: {
+					Authorization: "",
+					"Content-Type": "application/json",
+					Accept: "application/json",
+				},
+			});
+			const data = await res.json();
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
+	}
 }
