4f4797266bad0f2458cc6e03489b5c6407107348
Omar Elbaroudy <elbaroudyomar@gmail.com>
2021-01-15 14:48:11 +0200

    testing refresh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const path = require("path");
+//const path = require("path");
 
 const hrRouter = require("./routes/hrRouter");
 const hodRouter = require("./routes/hodRouter");
@@ -20,7 +20,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "client", "build")));
+//app.use(express.static(path.join(__dirname, "client", "build")));
 
 const loadTokens = async function () {
 	try {
@@ -74,11 +74,11 @@ mongoose
 		app.use("", instructorRouter);
 		app.use("", coordinatorRouter);
 
-		app.get("*", (req, res) => {
-			res.sendFile(
-				path.join(__dirname, "client", "build", "./client/src/views/index.html")
-			);
-		});
+		// app.get("*", (req, res) => {
+		// 	res.sendFile(
+		// 		path.join(__dirname, "client", "build", "./client/src/views/index.html")
+		// 	);
+		// });
 
 		app.listen(port, () => {
 			console.log("connected");
